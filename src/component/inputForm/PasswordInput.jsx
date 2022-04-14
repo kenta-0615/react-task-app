5d37e1f0eed11830b0f2f5f946dc21a30f90cd36
笠井　健太 <kentakasai@This-is-kentas-MacBook-Air-device.local>
2022-04-14 21:16:27 +0900

    Re add

--- src/component/inputForm/PasswordInput.jsx
+++ src/component/inputForm/PasswordInput.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import { useState, memo } from "react";
 
 export const PasswordInput = memo(() => {
-    const [password, setPassword] = useState;
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [state, setState] = useState('');
+    const handlePasswordChange = event => {
+        setPassword(event.target.value);
+    };
+    const handleSave = () => {
+        console.log(password);
+    }
 
     const passwordErrorMessage = () => {
         if(password.length <= 0) return 'パスワードを入力してください'
@@ -14,9 +22,9 @@ export const PasswordInput = memo(() => {
             <label>
                 PASS:
             </label>
-            <input type="password" name="password" id="password" value={password} onChange={(e) => {setPassword(e.target.value)} } onClick={() => {}} />
+            <input type="password" name="password" id="password" value={password} onChange={handlePasswordChange} />
             {passwordErrorMessage().length > 0 && <p>{passwordErrorMessage}</p>}
-            <button>Save</button>
+            <button onClick={handleSave}>Save</button>
         </form>
     )
 });
