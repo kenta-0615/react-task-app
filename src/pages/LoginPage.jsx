6b63c431cd5deedfb510f9d6fd026e04bebf5243
taishi1116 <shinoda1116.miwa@gmail.com>
2022-04-24 11:22:59 +0900

    textNameコンポーネントの修正

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { TextName } from "../component/inputForm/TextName";
 import { EmailAddressInput } from "../component/inputForm/EmailAddressInput";
 import { PasswordInput } from "../component/inputForm/PasswordInput";
@@ -5,16 +6,35 @@ import { TelephoneNumberInput } from "../component/inputForm/TelephoneNumberInpu
 import { SubmitButton } from "../component/Button/SubmitButton";
 import { memo } from "react";
 
-
 export const LoginPage = memo(() => {
+  const [name, setName] = useState("");
+  console.log(name);
 
-    return(
-        <div>
-            <TextName />
-            <EmailAddressInput />
-            <PasswordInput />
-            <TelephoneNumberInput />
-            <SubmitButton />
-        </div>
-    );
+  const nameErrorMessage = () => {
+    if (name.length <= 0) return "必須入力です";
+    console.log(name.length);
+    if (name.length < 6 || name.length > 38) return "半角英数字のみ";
+    console.log(name.length);
+    if (!/^[0-9a-zA-Z]*$/.test(name)) return "半角英数字ではありません";
+    console.log(!/^[0-9a-zA-Z]*$/);
+    return "";
+  };
+
+  const changeName = (e) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <div>
+      <TextName
+        name={name}
+        onChange={changeName}
+        errorMessage={nameErrorMessage}
+      />
+      <EmailAddressInput />
+      <PasswordInput />
+      <TelephoneNumberInput />
+      <SubmitButton />
+    </div>
+  );
 });
