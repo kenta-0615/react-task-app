57b3fc052d640388913fd7cf11fd17998e9cd6c1
笠井　健太 <kentakasai@This-is-kentas-MacBook-Air-device.local>
2022-04-26 07:37:52 +0900

    ページの格納場所のドロップダウンの実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,15 @@ import React from "react";
 import { Routes, Route} from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { Task2Page } from "./pages/Task2Page";
+import { PageMenu } from "../src/component/WebPageLayout/PageMenu"
 import { memo } from "react";
 
 export const App = memo(() => {
+
   return(
     <>
-    <h1>Hello React Router v6</h1>
-    <ul>
-      <li>
-        <a href="/">LoginPage</a>
-      </li>
-      <li>
-        <a href="/Task2Page">Task2Page</a>
-      </li>
-    </ul>
+    <PageMenu />
+   
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/Task2Page" element={<Task2Page message="Hello Task2Page" />}/>
